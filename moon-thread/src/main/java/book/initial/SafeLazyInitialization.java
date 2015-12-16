bf721680fe338da7616d58f10716299ba965a18d
moon <379222290@qq.com>
2015-12-16 10:31:11 +0800

    add thread book-initial

---
+++ moon-thread/src/main/java/book/initial/SafeLazyInitialization.java
@@ -0,0 +1,21 @@
+package book.initial;
+
+public class SafeLazyInitialization {
+    private static Resource resource;
+
+    public synchronized static Resource getInstance() {
+        if (resource == null)
+            resource = new Resource();
+        return resource;
+    }
+
+    static class Resource {
+    }
+}
+/*
+
+
+
+
+
+ */
---
+++ moon-thread/src/main/java/book/initial/UnsafeLazyInitialization.java
@@ -0,0 +1,14 @@
+package book.initial;
+
+public class UnsafeLazyInitialization {
+    private static Resource resource;
+
+    public static Resource getInstance() {
+        if (resource == null)
+            resource = new Resource(); // unsafe publication
+        return resource;
+    }
+
+    static class Resource {
+    }
+}
