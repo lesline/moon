b88109c664a9a536648b138fd64562601507a41e
moon <379222290@qq.com>
2015-12-26 00:45:19 +0800

    add thread

--- moon-thread/src/main/java/book/initial/DoubleCheckedLocking.java
+++ moon-thread/src/main/java/book/initial/DoubleCheckedLocking.java
@@ -1,7 +1,12 @@
 package book.initial;
 
+/**
+ * 双重检查加锁不是线程安全的,加volatile后可以保证线程安全
+ */
+
 public class DoubleCheckedLocking {
-    private static Resource resource;
+    private static Resource resource;//线程不安全
+    //private volatile static Resource resource;--线程安全的
 
     public static Resource getInstance() {
         if (resource == null) {
